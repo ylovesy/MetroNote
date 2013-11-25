5cfb41a219340bb4e0c1a79eedfefd65f26d626d
ylovesy <y20160624y@gmail.com>
2013-11-25 21:24:01 +0800

    first commit

---
+++ MetroNote/MetroNote/MNYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MNYAppDelegate.h
+//  MetroNote
+//
+//  Created by YY on 11/25/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MNYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MetroNote/MetroNote/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MetroNote
+//
+//  Created by YY on 11/25/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MNYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MNYAppDelegate class]));
+    }
+}
